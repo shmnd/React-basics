7dfff307323e02abee5c30c9c88471adab042d2c
Shamnad <hamzashamnad123@gmail.com>
2025-08-25 19:53:59 +0530

    count + adivice

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,31 +1,41 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
 
-  const [advice, setAdvice] = useState()
+  const [advice, setAdvice] = useState("")
   const [count, UpdateCount] = useState(0)
 
-  async function GetAdviceApp() {
+  const GetAdviceApp = async () => {
 
     const result = await fetch("https://api.adviceslip.com/advice");
     const data = await result.json()
+    console.log(data.slip.id)
     setAdvice(data.slip.advice);
     UpdateCount(c => c+1);
-    console.log(count,'countttttttttt')
 
   }
+
+  useEffect( function App() {
+    GetAdviceApp();
+  }, []);
 
   return (
     <div>
       <h1>Hello World!</h1>
       <h2>Today advice is : {advice}</h2>
       <button onClick={GetAdviceApp}>Get Free Adivices</button>
-      <p>
-        You read <strong>{count}</strong> Advices
-      </p>
+      <Message count = {count} />
     </div>
     
   );
 }
 
+function Message (props) {
+  return (
+    <p>
+        You read <strong>{props.count}</strong> Advices so far <br />
+        my name is {props.name}
+    </p>
+  )
+}
