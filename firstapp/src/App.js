a81307ae8ba565c4a664a607a1421c29f89019c3
Shamnad <hamzashamnad123@gmail.com>
2025-08-25 13:07:03 +0530

    normal async function

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,8 +1,31 @@
 
-function App() {
+import { useState } from "react";
+
+export default function App() {
+
+  const [advice, setAdvice] = useState()
+  const [count, UpdateCount] = useState(0)
+
+  async function GetAdviceApp() {
+
+    const result = await fetch("https://api.adviceslip.com/advice");
+    const data = await result.json()
+    setAdvice(data.slip.advice);
+    UpdateCount(c => c+1);
+    console.log(count,'countttttttttt')
+
+  }
+
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <h1>Hello World!</h1>
+      <h2>Today advice is : {advice}</h2>
+      <button onClick={GetAdviceApp}>Get Free Adivices</button>
+      <p>
+        You read <strong>{count}</strong> Advices
+      </p>
+    </div>
+    
   );
 }
 
-export default App;
